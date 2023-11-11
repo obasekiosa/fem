53f016fee6f8e3830cad8552afcfc8a0f4af6d0e
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-11-11 13:18:44 +0100

    switch to hashrouter

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import {
-    createBrowserRouter,
+    createHashRouter,
 } from "react-router-dom";
 import App from "./App";
 import { Demo } from "./challenges";
@@ -7,7 +7,7 @@ import Error from "./error";
 
 const ROOT_PATH = "/fem";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <App />,
@@ -17,8 +17,6 @@ const router = createBrowserRouter([
         path: "/test",
         element: <Demo />
     }
-], {
-    basename: ROOT_PATH
-});
+]);
 
 export default router;
