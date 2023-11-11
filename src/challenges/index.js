fdae47bc729402d5b434cae24aab25c1cc4762f9
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-11-11 13:54:58 +0100

    Update Demo page

--- src/challenges/index.js
+++ src/challenges/index.js
@@ -4,6 +4,6 @@
 export function Demo() {
 
     return  <div style={{backgroundColor: "green", width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "20rem"}}>
-        Hello
+        Hello to you
     </div>
 }
