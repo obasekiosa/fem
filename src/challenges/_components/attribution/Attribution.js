f8dabc98f2614ddc87c03f1173dd7225e5b6afbc
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-11-11 17:52:07 +0100

    Fix non referrer link bug

--- src/challenges/_components/attribution/Attribution.js
+++ src/challenges/_components/attribution/Attribution.js
@@ -5,7 +5,7 @@ export function Attribution({name, authorUrl}) {
 
     return (
         <div className="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
             Coded by <a href={authorUrl || "#"}>{name || "Your name here"}</a>.
         </div>
     )
